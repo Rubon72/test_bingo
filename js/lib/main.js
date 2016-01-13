6d6ecd4c102a87c0ca59d2bf4d3f43f21c28a056
Mikhail Baudzei <Rubon72@gmail.com>
2016-01-13 18:01:18 +0300

    final

--- js/lib/main.js
+++ js/lib/main.js
@@ -1,13 +1,47 @@
 
 $(document).ready(function(){
+
+    /*select*/
     $("#select-town").selectmenu();
+
+
+    /*menu*/
+    $('.header__menu_but').click(function(){
+        $('.overlay').addClass('active');
+        $('.overlay').css({'height':$(document).height()});
+        $('.menuoverlay').addClass('active');
+
+    });
+    $('.overlay, .menuoverlay__close').click(function(){
+        $('.overlay').removeClass('active');
+        $('.menuoverlay').removeClass('active');
+        $('.header__menu_but').removeClass('active');
+    });
+
+
 });
 
 $(window).load(function(){
 
+    /*index carousel*/
     $('#indexWin8Car').lemmonSlider({
         infinite: true
     });
+
+    /*index slider*/
+    $("#owl-car").owlCarousel({
+        items:1,
+        autoPlay:true,
+        navigation : true,
+        navigationText : ["",""],
+        afterAction : afterAction
+
+    });
+    $('.owl-buttons').append('<div class="islidercount"><div class="islidercount__big">1</div><div class="islidercount__small">/<span>'+$('#owl-car .index_owl_one').length+'</span></div></div>');
+
+    function afterAction(){
+        $('.islidercount__big').text(this.owl.currentItem+1);
+    }
 });
 
 
